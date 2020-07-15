752e6f47f5cced67e9827251dc45b0e1c5135df6
Shubh Gaur <32275143+JudgedSauce7@users.noreply.github.com>
2020-07-15 23:38:26 +0530

    Issue Fixed

--- cleandoc/settings.py
+++ cleandoc/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'li1nmjyuw24&%2y$7+81cvs$$lr3e@c=3%i&@8_a4zhdx5sdlc'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
